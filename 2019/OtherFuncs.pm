2c2d71223995f84eece83a2fbd7ddca3df5f950c
Tom Browder <tom.browder@gmail.com>
2019-11-28 07:27:15 -0600

    add new Perl module

---
+++ 2019/OtherFuncs.pm
@@ -0,0 +1,19 @@
+package PicFuncs;
+
+use feature 'say';
+use strict;
+use warnings;
+
+use Perl6::Export::Attrs;
+use Carp;
+
+use lib '.';
+use G;
+
+sub collect_pic_info :Export(:DEFAULT) {
+}
+
+
+
+##### obligatory 1 return for a package #####
+1;
